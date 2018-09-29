ea57498f329c6192e62515cf4f4ba2007a6a6e8b
lijingxiang888 <924415666@qq.com>
2018-09-29 17:28:24 +0800

    优化banner尺寸

--- js/index.js
+++ js/index.js
@@ -24,4 +24,41 @@ $(function () {
     }
     myshow();
     $(window).scroll(myshow);
+
+    //获取最新动态和行业资讯
+    var latesUrl = './data/latestnews.json',
+        bussiUrl = './data/businessnews.json';
+
+    function getNews(url) {
+        $.ajax({
+            url:url,
+            type:'get',
+            async:false,
+            dataType:'json',
+            success:bindHTML
+        });
+
+        function bindHTML(data) {
+            if (/latestnews/.test(url)){
+                var mainHTML = $('#mainTemplate').html(); //获取模板的html结构
+                var result = ejs.render(mainHTML,{mainData:data}); //向模板里填数据
+                $('#latest').html(result); //现在的result又有结构又有数据
+                $('#latest li').click(function () { //li存在之后绑定点击事件
+                    var pageNum = $(this).index();
+                    window.location.href = './detail.html?pick=0&pagenum=' + pageNum
+                })
+            }else {
+                var mainHTML = $('#mainTemplate2').html();
+                var result = ejs.render(mainHTML,{mainData:data});
+                $('#business').html(result);
+
+                $('#business li').click(function () {
+                    var pageNum = $(this).index();
+                    window.location.href = './detail.html?pick=1&pagenum=' + pageNum
+                })
+            }
+        }
+    }
+    getNews(latesUrl);
+    getNews(bussiUrl);
 });
